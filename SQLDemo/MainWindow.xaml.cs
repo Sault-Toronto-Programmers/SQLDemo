4d4086998af5fa8c1a728a393fb8ed5050192782
rangapp-droid <rekprakash@gmail.com>
2021-08-27 16:34:34 -0400

    Integrating data grid with SQL

--- SQLDemo/MainWindow.xaml.cs
+++ SQLDemo/MainWindow.xaml.cs
@@ -39,16 +39,48 @@ namespace SQLDemo
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine(SupId.Text);
-            MySqlCommand cmd = new MySqlCommand("select Company from ccap_suppliers where Supplier_id=@SupID", conn);
-            cmd.Parameters.AddWithValue("@SupID", SupId.Text);
-            string name = Convert.ToString(cmd.ExecuteScalar());
-            SupName.Text = name;
+            try { 
+                List<Company> companies = new List<Company>();
+                MySqlCommand cmd = new MySqlCommand("select Company, email_address, Phone from ccap_suppliers where city=@City", conn);
+                cmd.Parameters.AddWithValue("@City", City.Text);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows) { 
+                    while (reader.Read())
+                    {
+                        Company company = new Company(reader[0].ToString(), reader[1].ToString(), reader[2].ToString());
+                        companies.Add(company);
+                    }
+                    CompanyList.ItemsSource = companies;
+                }
+                else
+                {
+                    MessageBox.Show("No companies found in the selected city");
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
 
         private void Window_Unloaded(object sender, RoutedEventArgs e)
         {
             conn.Close();
+        }
+    }
+
+    class Company
+    {
+        public string CompanyName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+
+        public Company(string _company, string _email, string _phone)
+        {
+            CompanyName = _company;
+            email = _email;
+            phone = _phone;
         }
     }
 }
